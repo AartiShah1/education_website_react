4f7e3fa7a529e28feca0f9ff4a3bc3467990bbfb
AartiShah1 <aartishah9855@gmail.com>
2025-06-26 13:25:20 +0545

    vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import Landing_page from './pages/Landing_page'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Landing_page from './pages/Landing_page';
 import About from './pages/about';
 import Courses from './pages/Courses';
 import Contact from './pages/Contact';
 
 function App() {
- 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/education_website_react">
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing_page />} />
         <Route path="/about" element={<About />} />
-              <Route path="/courses" element={<Courses />} />
-                    <Route path="/contact" element={<Contact />} />
-
-
-       
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/education_website_react/', // 👈 very important!
   plugins: [react(),
       tailwindcss(),
   ],
